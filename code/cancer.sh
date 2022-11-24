4e31f4e2c7e7fca1a487d5847297eea547e56263
FarinosJhon <jhon.farinos@est.ikiam.edu.ec>
2022-11-23 20:12:44 -0500

    Farinos Jhon has determinado los cánceres del tipo carcinoma.

--- code/cancer.sh
+++ code/cancer.sh
@@ -1,1 +1,6 @@
-vacio.
+
+#Cantidad de registros de cada tipo de cáncer
+cat ../data/cancermine.csv | cut -d "," -f 3 | tail -n +2 | sort | uniq -c | sort -n>../data/cancer.txt
+
+#Cantidad de registros del término carcinoma.
+cat ../data/cancermine.csv | cut -d "," -f 3 | tail -n +2 | grep carcinoma | wc -l>../data/carcinoma.txt
