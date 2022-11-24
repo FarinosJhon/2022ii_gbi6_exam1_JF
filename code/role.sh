6ed17d19a1662d763b50365f7ed356bd9e090c7b
FarinosJhon <jhon.farinos@est.ikiam.edu.ec>
2022-11-23 19:43:37 -0500

    Farinos Jhon ha contado los roles de los genes.

--- code/role.sh
+++ code/role.sh
@@ -1,1 +1,14 @@
-..
+echo "Resultados Pregunta 1"
+echo " "
+#Número de registros de cada rol del gen
+echo "Número de registros de cada rol del gen:"
+cat ../data/cancermine.csv | cut -d "," -f 1 | tail -n +2 | wc -l
+
+#Especie y conteo más alto
+echo "Especie y conteo más alto:"
+cat ../data/cancermine.csv | cut -d "," -f 1 | tail -n +2 | sort -n | uniq -c | sort -r | head -n 1
+
+#Especie y conteo más bajo
+echo "Especie y conteo más bajo:"
+cat ../data/cancermine.csv | cut -d "," -f 1 | tail -n +2 | sort -n | uniq -c | sort -r | tail -n 1
+
