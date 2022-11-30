8711e43d476510cd5957c01fdf8bce6d7bf64fbd
FarinosJhon <jhon.farinos@est.ikiam.edu.ec>
2022-11-30 00:59:46 -0500

    Farinos Jhon ha generado archivos de cancer en la rama canceres

--- canceres.sh
+++ canceres.sh
@@ -1,1 +1,9 @@
-.
+#Para tumor_supressor
+cat data/cancermine.csv | cut -d "," -f 1 | tail -n +2 | sort -n | uniq -c | tail -n 1 > Tumor_supresor.txt
+
+#Oncogene
+cat data/cancermine.csv | cut -d "," -f 1 | tail -n +2 | sort -n | uniq -c | tail -n 2 | head -n 1 > Oncogene.txt
+
+#Driver
+cat data/cancermine.csv | cut -d "," -f 1 | tail -n +2 | sort -n | uniq -c | head -n 1 > Driver.txt
+
